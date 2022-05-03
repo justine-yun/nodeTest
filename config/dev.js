1f66c8014131afd3a9a18e18a034dd6979fc935f
beepbipbp <riskyjunk@gmail.com>
2022-05-03 18:13:46 +0900

    20220503 1813

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://riskyjunk:w3h1kj7m@boilerplate.dgfhf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 5000;
+const { User } = require("./models/User");
+const config = require("./config/key")
 
-mongoose.connect("mongodb+srv://riskyjunk:w3h1kj7m@boilerplate.dgfhf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI)
     .then(() => console.log("MongoDB Connected..."))
     .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-    res.send('Hello World! 안녕하세요!');
+    res.send('Hello World!');
+});
+
+app.post("/register", (req, res) => {
+    const user = new User;
+    user.save((err, userInfo) => {
+        if(err){
+            return res.json({success: false, err});
+        }
+        return res.status(200).json({success: true});
+    });
 });
 
 app.listen(port, () => {
