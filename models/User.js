59d1b19bd54cec4928592330365a43a4200fbab8
beepbipbp <riskyjunk@gmail.com>
2022-05-11 02:14:21 +0900

    20220511 0214

--- models/User.js
+++ models/User.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         maxLength: 50
@@ -26,6 +29,65 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+userSchema.pre("save", function(next){
+    let user = this;
+    if(user.isModified("password")){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err){
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err){
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    }
+    else{
+        next();
+    }
+});
+
+userSchema.methods.comparePassword = function(plainPassword, callback){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err){
+            return callback(err);
+        }
+        callback(null, isMatch);
+        // 이 부분 잘 모르겠는데 나중에 한 번 살펴보기
+        // if(err) return callback(err),
+        //     callback(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(callback){
+    let user = this;
+
+    let token = jwt.sign(user._id.toHexString(), "secretToken");
+    user.token = token;
+    user.save(function(err, user){
+        if(err){
+            return callback(err);
+        }
+        callback(null, user);
+    });
+}
+
+userSchema.statics.findByToken = function(token, callback){
+    let user = this;
+
+    jwt.verify(token, "secretToken", function(err, decoded){
+        user.findOne({ "_id": decoded, "token": token }, function(err, user){
+            if(err){
+                return callback(err);
+            }
+            callback(null, user);
+        });
+    });
+}
 
 const User = mongoose.model("User", userSchema);
 
